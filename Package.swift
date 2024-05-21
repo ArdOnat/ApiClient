151ef1e19e248ec335d7ee07d35b55e97bdfe2e2
Arda Onat <arda.onat@ceng.deu.edu.tr>
2024-05-21 23:28:44 +0300

    Update CoreModule to 1.4.1

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/ArdOnat/CoreModule.git", .exact("1.4.0"))
+        .package(url: "https://github.com/ArdOnat/CoreModule.git", .exact("1.4.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
